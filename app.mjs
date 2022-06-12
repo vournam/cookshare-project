36edbeaedb4bab7da79e7ac1498cf62f56c3a756
vournam <up1060891@upnet.gr>
2022-06-12 17:34:31 +0300

    db server

--- app.mjs
+++ app.mjs
@@ -3,10 +3,12 @@ import handlebars from 'hbs';
 import exphbs from "express-handlebars";
 import path from "path";
 import session from "express-session";
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// import * as model from './model/model_pg.js';
+
+import * as model from '../model/model_pg.mjs';
 
 // Create express server
 const app = express();
@@ -21,85 +23,30 @@ app.use(express.static(path.join(__dirname, "public")));
 // Parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 
-// Router
-const router = express.Router();
-//load the router 'routes' on '/'
-app.use(router); 
+// //Διαδρομές. Αντί να γράψουμε τις διαδρομές μας εδώ, τις φορτώνουμε από ένα άλλο αρχείο
+// import routes from './routes/cookshare-routes.mjs'
+// //και τώρα χρησιμοποιούμε αυτές τις διαδρομές
+// app.use('/', routes);
 
-// Homepage - non registered with /
-router.route('/').get( (req,res) => {
-    res.render('nonRegHome');
-} );
+// Προσθήκη του express-session middleware
+app.use(session({
+    name: process.env.SESS_NAME,
+    secret: process.env.SESSION_SECRET || "PynOjAuHetAuWawtinAytVunar", // κλειδί για κρυπτογράφηση του cookie
+    resave: false, // δεν χρειάζεται να αποθηκεύεται αν δεν αλλάξει
+    saveUninitialized: false, // όχι αποθήκευση αν δεν έχει αρχικοποιηθεί
+    cookie: {
+      maxAge: 2*60*60*1000, //TWO_HOURS χρόνος ζωής του cookie σε ms or process.env.SESSION_LIFETIME
+      sameSite: true
+    }
+}));
 
-// Homepage - non registered
-router.route('/nonRegHome').get( (req,res) => {
-    res.render('nonRegHome');
-} );
-
-// Homepage - registered
-router.route('/regHome').get( (req,res) => {
-    res.render('regHome');
-} );
-
-// Search - non registered
-router.route('/nonRegSearch').get( (req,res) => {
-    res.render('nonRegSearch');
-} );
-
-// Search - registered
-router.route('/regSearch').get( (req,res) => {
-    res.render('regSearch');
-} );
-
-// About - non registered
-router.route('/nonRegAbout').get( (req,res) => {
-    res.render('nonRegAbout');
-} );
-
-// About - registered
-router.route('/regAbout').get( (req,res) => {
-    res.render('regAbout');
-} );
-
-// Contact - non registered
-router.route('/nonRegContact').get( (req,res) => {
-    res.render('nonRegContact');
-} );
-
-// Contact - registered
-router.route('/regContact').get( (req,res) => {
-    res.render('regContact');
-} );
-
-// Sign-in
-router.route('/signIn').get( (req,res) => {
-    res.render('signIn');
-} );
-
-// Recipe - non registered
-router.route('/nonRegRecipe').get( (req,res) => {
-    res.render('nonRegRecipe');
-} );
-
-// Recipe - registered
-router.route('/regRecipe').get( (req,res) => {
-    res.render('regRecipe');
-} );
-
-//// Add recipe - registered
-router.route('/regAddRecipe').get( (req,res) => {
-    res.render('regAddRecipe');
-} );
-
-// Profile - registered
-router.route('/regProfile').get( (req,res) => {
-    res.render('regProfile');
-} );
-
-// My recipe - registered
-router.route('/regMyRecipe').get( (req,res) => {
-    res.render('regMyRecipe');
-} );
+const redirectHome = (req, res, next) => {
+console.log('redirect...', req.session)
+if (!req.session.userID) {
+res.redirect('/');
+} else {
+next();
+} };
 
 // Port
 const PORT = process.env.PORT || 3003
@@ -107,3 +54,85 @@ app.listen(PORT, ()=> {
     console.log(`Συνδεθείτε στη σελίδα: http://localhost:${PORT}`);
 });
 
+// GET /about
+app.get("/nonRegAbout", (req, res) => {
+    console.log("GET /nonRegAbout session=", req.session);
+    res.render("nonRegAbout");
+  });
+
+// GET /contact
+app.get("/nonRegContact", (req, res) => {
+    console.log("GET /nonRegContact session=", req.session);
+    res.render("nonRegContact");
+  });
+
+
+// GET /create
+app.get("/regAddRecipe", (req, res) => {
+    console.log("GET /regAddRecipe, session=", req.session)
+    res.render("regAddRecipe", { data: {} });
+});
+
+// POST /create
+app.post("/regAddRecipe", (req, res) => {
+console.log("POST /regAddRecipe session=", req.session);
+// const userID = req.session.userID;
+// console.log(userID);
+const newRecipe = {"title":req.body.title, "img":req.body.img, "ingredient":req.body.ingredient, "description":req.body.description, "time":req.body.time, "portions":req.body.portions, "level":req.body.level } 
+model.newRecipe(newRecipe,
+    (err, data)=> {
+    if (err)
+        return console.error(err.message);
+    else
+    res.redirect("/regAllRecipes");
+    }); 
+});
+
+// // GET /edit/:bookID
+// app.get("/edit/:bookID", (req, res) => {
+//     console.log("GET /edit/:id session=", req.session);
+//     const id = req.params.bookID;
+//     if(id){
+//       console.log('edit', id)
+//       model.findBook(id, (err, row) => {
+//         if (err) {
+//           res.send(err);
+//         } else {
+//           console.log('get /edit/id book to edit', row[0]);
+//           res.render("edit", { data: row[0] });
+//         }
+//       });
+//     }
+//   });
+  
+//   // POST /edit/:bookID
+//   app.post("/edit/:id", (req, res) => {
+//     console.log("POST /edit/:id session=", req.session);
+//     const id = req.params.id;
+//     // const book = [req.body.title, req.body.author, req.body.comment, id];
+//     const book = {"title":req.body.title, "author":req.body.author, "comment":req.body.comment, "bookID":id, "user": req.session.userID}
+//     model.updateBook(book, (err, data) => {
+//       console.log('in POST', err, data)
+//       if(err){
+//         return console.error(err.message);
+//       }
+//       else {
+//         res.redirect("/books");
+//       }  
+//     });
+//   });
+  
+//   // GET /delete/:bookID
+//   app.get("/delete/:id", redirectHome, (req, res) => {
+//     console.log("GET /delete/:id session=", req.session);
+//     const id = req.params.id;
+//     console.log("GET /delete/:id", id);
+//     model.findBook(id, (err, row) => {
+//       if (err) {
+//         return console.error(err.message);
+//       } 
+//       console.log("TO BE DELETED...", row);
+//       res.render("delete", { data: row[0] });
+//     });
+//     console.log('END of GET /delete/:id')
+//   });
---
+++ model/model_pg.mjs
@@ -0,0 +1,223 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env)
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL, //μεταβλητή περιβάλλοντος
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+// const pool = new pg.Pool();  // to connect to the local database
+
+async function connect() {
+    try {
+        const client = await pool.connect();
+        return client
+    }
+    catch(e) {
+        console.error(`Failed to connect ${e}`)
+    }
+}
+
+async function getMyRecipes(userID, callback) {
+    // ανάκτηση όλων των βιβλίων του χρήστη από τη βάση δεδομένων
+    const sql = `SELECT * FROM "recipe" WHERE "belongs_to_user_user_id" = '${userID}' ORDER BY "title";`;
+    try {
+        const client = await connect();
+        const res = await client.query(sql)
+        await client.release()
+        callback(null, res.rows) // επιστρέφει array
+    }
+    catch (err) {
+        callback(err, null);
+    }
+}
+
+//// testing
+// getMyBooks(2,  (err, rows) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('.....-->', rows);
+//   });
+
+async function newRecipe (recipe, callback) {
+    console.log('to insert...', recipe)
+    const sql = `INSERT INTO "recipe" ("title", "img", "ingredient", "description", "time", "portions", "level") 
+        VALUES ('${recipe.title}', '${recipe.img}', '${recipe.ingredient}', '${recipe.description}', '${recipe.time}', '${recipe.portions}', '${recipe.level}');`;
+    try {
+        const client = await connect();
+        const res = await client.query(sql)
+        await client.release()
+        callback(null, res.rows) // επιστρέφει array
+    } 
+    catch (err) {
+            callback(err, null);
+        }
+    }
+
+///// testing
+// newBook( {"title": "the best book", "author": "me who else", "comment":"", "user": 2}, (err, rows) => {
+//     if (err) {
+//       return console.error(err.message);
+//     } else console.log(rows)
+//   });
+
+async function findRecipe(recipeIngredient, recipeCategory, callback) {
+    console.log('findRecipeWithBasicIngredient', recipeIngredient)
+    const sql = `SELECT * FROM "recipe" WHERE "ingredient" = '${recipeIngredient}'`; //Add the category
+    try {
+        const client = await connect();
+        const res = await client.query(sql)
+        await client.release()
+        callback(null, res.rows) // επιστρέφει array
+    } 
+    catch (err) {
+            callback(err, null);
+        }
+    }
+
+// // testing
+// findBook(7, (err, data) => {
+//     if (err) {
+//         return console.log(err)
+//     }
+//     else console.log(data)
+// });
+
+async function updateRecipe (recipe, callback) {
+    const sql = `UPDATE "recipe"
+        SET "title" = '${recipe.title}', img = '${recipe.img}', ingredient = '${recipe.ingredient}', description = '${recipe.description}', time = '${recipe.time}', portions = '${recipe.portions}', level = '${recipe.level}'
+        WHERE ("recipe_id" = '${recipe.recipe_id}');`;
+    try {
+        const client = await connect();
+        const res = await client.query(sql)
+        await client.release();
+        console.log(`Row(s) updated`);
+        callback(null, res.rows) // επιστρέφει array
+    } 
+    catch (err) {
+        console.log(e);
+        callback(e, null);
+        }
+    }
+
+/// testing
+// updateBook({"bookID":8,"title":"Another good book","author":"someone else", "comment":""}, (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data)
+// })
+
+async function deleteRecipe (recipe, callback) {
+    const sql = `DELETE FROM "recipe"
+        WHERE  "recipe_id" = '${recipe.recipe_id}';`;
+    try {
+        const client = await connect();
+        const res = await client.query(sql)
+        await client.release();
+        console.log(`Recipe deleted`);
+        callback(null, res.rows) // επιστρέφει array
+    } 
+    catch (err) {
+        console.log(err);
+        callback(err, null);
+        }
+    }
+
+/// testing
+// deleteBook(7, (err, data) => {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log(data);
+// })
+
+// async function insertUser (userName, callback) {
+//     // εισαγωγή νέου χρήστη, και επιστροφή στο callback της νέας εγγραφής
+//     const sql = `INSERT INTO "user"("alias") VALUES ('${userName}') RETURNING "userID";`
+//     console.log('to insert...', sql)
+//     // how to return the autoincremented value of an inserted record: https://stackoverflow.com/questions/37243698/how-can-i-find-the-last-insert-id-with-node-js-and-postgresql
+//     try {
+//         const client = await connect();
+//         const res = await client.query(sql);
+//         await client.release();
+//         console.log(`user inserted`);
+//         callback(null, [{"userName": userName, "userID": res.rows[0].userID}]) // επιστρέφει array
+//     } 
+//     catch (err) {
+//         console.log(err);
+//         callback(err, null);
+//         }
+//     }
+
+///// testing
+
+// insertUser("Μαρικούλα", (err, data) => {
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(data);
+// })
+        
+async function findUser(userID=null, userName=null, callback) {
+    // εύρεση χρήστη με βάση τον κωδικό ή το όνομά του.
+    // χωρίς μυστικό κωδικό για λόγους απλότητας
+    const sql = (userID) ? `SELECT * FROM "user" WHERE "user_id" = '${userID}';` : 
+    `SELECT * FROM "user" WHERE "alias" = '${userName}';`;  
+    console.log('new sql...', sql) 
+    try {
+        const client = await connect();
+        // console.log('theclient...', client)
+        const res = await client.query(sql);
+        if (res.rows.length === 0) {
+            // αν ο χρήστης δεν υπάρχει, πρέπει να δημιουργηθεί
+            await client.release();
+            await insertUser(userName, (err, newUser) => {
+                console.log("newuser", newUser);
+                if (err) {
+                    callback(err, null);
+                } else
+                    findUser(userID, userName, callback);
+            });
+        }
+        else {
+            console.log(`user found`);
+            callback(null, [{"userName": userName || res.rows[0].userName, "userID": userID || res.rows[0].userID}]) // επιστρέφει array
+        }
+    } 
+    catch (err) {
+        console.log(err);
+        callback(err, null);
+        }
+}
+
+// //// test#1
+// findUser(null, "Νίκος", (err, data) => {
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(data)
+// })
+
+// //// test#2
+// findUser(null, "Jhon Jhon", (err, data) => {
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(data)
+// })
+
+
+// const  query = (text, params, callback) => {
+//     const db = new sqlite3.Database(db_name);
+//     return db.query(text, params, callback)
+//   }
+
+export { getMyRecipes, newRecipe, findRecipe, updateRecipe, deleteRecipe, findUser };
+
